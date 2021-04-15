5f4324d43f309e17742291543b9be9c72379df26
realphilip <pajach@pm.me>
2021-04-15 11:17:55 +0200

    initial spring boot hello world commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # your-recipes
 Personalized website for you to store your favorite recipes
+
+Pairprogrammed by: Philip Jach 574366 @realphilip
+
+Huy Nam Nguyen 566103 @huynamnguyen
---
+++ src/main/java/htw/berlin/webapp/HelloController.java
@@ -0,0 +1,14 @@
+package htw.berlin.webapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+        @RequestMapping("/")
+        public String index() {
+            return "Welcome to Your Recipes";
+
+        }
+    }
