ccb11d3367423ea3b99110cd322ed768a0e168ba
Huy Nam Nguyen <41015677+HNamNg@users.noreply.github.com>
2021-05-07 20:38:08 +0200

    test

--- src/main/java/htw/berlin/webapp/HelloController.java
+++ src/main/java/htw/berlin/webapp/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+#test
         @RequestMapping("/")
         public String index() {
             return "Welcome to Your Recipes";
